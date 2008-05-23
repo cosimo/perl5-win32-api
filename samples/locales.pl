4542ba1e0e6ee24fde5901d2e1962bf608f0229f
Cosimo Streppone <cosimo@cpan.org>
2008-05-23 21:17:00 +0000

    Create directories to load project into.
      * trunk/Win32-API: New directory.

--- samples/locales.pl
+++
@@ -1,8 +0,0 @@
-use Win32::kernel32;
-
-for $locale (0..2048) {
-    $lang = Win32::VerLanguageName($locale);
-    if($lang ne "Language Neutral") {
-        printf("%4d (%s) %s\n", $locale, $lang, Win32::GetCurrencyFormat(10000, $locale));
-    }
-}
